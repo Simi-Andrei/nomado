ac390e6b1912316f6476d6485aae67f5451a50b4
Simi-Andrei <andy_andrei9572@yahoo.com>
2025-12-11 13:19:38 +0200

    deployed test

--- src/app/journeys/page.tsx
+++ src/app/journeys/page.tsx
@@ -1,20 +1,15 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { JourneysView } from "../views/journeys/journeys-view";
-import { ErrorBoundary } from "react-error-boundary";
+import { trpcCaller } from "@/trpc/server";
+import { redirect } from "next/navigation";
 
 const JourneyPage = async () => {
-  const queryClient = getQueryClient();
+  const caller = await trpcCaller();
+  const journeys = await caller.users.getUserJourneys();
 
-  await queryClient.prefetchQuery(trpc.users.getUserJourneys.queryOptions());
+  if (journeys.length === 0) {
+    redirect("/journeys/create");
+  }
 
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ErrorBoundary fallback={<p>Error</p>}>
-        <JourneysView />
-      </ErrorBoundary>
-    </HydrationBoundary>
-  );
+  redirect(`/journeys/${journeys[0].journeyId}`);
 };
 
 export default JourneyPage;
